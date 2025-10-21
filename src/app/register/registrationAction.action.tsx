50f4555545ed931e8fe1f587bf0e5a60cb278d47
M Karthik Pai <mkarthikpai25@gmail.com>
2025-10-21 20:47:45 +0530

    username and email validation

--- src/app/register/registrationAction.action.tsx
+++ src/app/register/registrationAction.action.tsx
@@ -3,6 +3,7 @@
 import { db } from "@/config/db";
 import { users } from "@/drizzle/schema";
 import argon2 from "argon2";
+import { eq, or } from "drizzle-orm";
 
 export const registrationAction = async (data: {
   name: string;
@@ -15,6 +16,17 @@ export const registrationAction = async (data: {
     //   console.log(formData.get("name"));
     //   console.log(Object.fromEntries(formData.entries()));
     const { name, userName, email, password, role } = data;
+
+    const [user] = await db
+      .select()
+      .from(users)
+      .where(or(eq(users.email, email), eq(users.userName, userName)));
+
+    if (user) {
+      if (user.email === email) {
+        return { status: "ERROR", message: "Email Already Exists" };
+      } else return { status: "ERROR", message: "Username Already Exists" };
+    }
 
     // Hashing Password
     const hashPassword = await argon2.hash(password);
