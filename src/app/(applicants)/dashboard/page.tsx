1c85801905c937250d96d2eb363ef778ddc72fbd
M Karthik Pai <mkarthikpai25@gmail.com>
2025-11-12 07:29:54 +0530

    Route Groups for dashboard

--- src/app/(applicants)/dashboard/page.tsx
+++ src/app/(applicants)/dashboard/page.tsx
@@ -1,9 +1,11 @@
+import { logoutUserAction } from "@/features/auth/server/auth.actions";
 import React from "react";
 
 const ApplicantDashboard = () => {
   return (
     <div>
       <h1>Hello Applicant Dashboard</h1>
+      <button onClick={logoutUserAction}>Logout</button>
     </div>
   );
 };
