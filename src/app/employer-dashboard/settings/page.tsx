326fd6255d078c3ed0aba653d3146d3e42145de5
M Karthik Pai <mkarthikpai25@gmail.com>
2025-11-26 08:26:37 +0530

    Employer Setting Form Development

---
+++ src/app/employer-dashboard/settings/page.tsx
@@ -0,0 +1,12 @@
+import EmployerSettingsForm from "@/features/employers/components/employer-setting-form";
+import React from "react";
+
+const EmployerSettings = () => {
+  return (
+    <div>
+      <EmployerSettingsForm />
+    </div>
+  );
+};
+
+export default EmployerSettings;
